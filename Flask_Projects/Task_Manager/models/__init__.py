ad3f5b5f6f79fc503e74771c7fdbbb021febc6f5
icareus1 <79919003+icareus1@users.noreply.github.com>
2023-11-07 16:30:52 +0000

    Delete Flask_Projects directory

--- Flask_Projects/Task_Manager/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-from .list_title import ListTitle
-from .tasks_content import TasksContent
--- Flask_Projects/Task_Manager/models/list_title.py
+++
@@ -1,9 +0,0 @@
-from . import db
-
-class ListTitle(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = db.Column(db.String(255), unique=True, nullable=False)
-    tasks = db.relationship('TasksContent', backref='list_title')
-    
-    def __repr__(self):
-        return self.name
